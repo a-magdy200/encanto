635c0f12187d4b8ab53cd23315942d5e4f14259e
Ahmed Magdy <ahmedmagdy.200m@gmail.com>
2022-04-30 14:40:35 +0200

    Update gym migration

--- database/migrations/2022_04_28_160633_create_gyms_table.php
+++ database/migrations/2022_04_28_160633_create_gyms_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('cover_image');
-            $table->foreignId("manager_id")->references("id")->on("users");
             $table->timestamps();
         });
     }
