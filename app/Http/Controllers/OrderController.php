9bdf52a76a7e89504cf42c12098ae432510b1239
ayaahassan <eng.ayaa2050@gmail.com>
2022-04-30 16:44:57 +0200

    create order view

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -14,3 +14,56 @@ class OrderController extends Controller
        return view('orders.index')->with(['items'=> $orders, 'title'=>$title, 'headings' => $headings]);
     }
 }
+/*
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+class UserController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    public function index()
+    {
+        $users = User::all();
+        return view('users', ['users' => $users]);
+    }
+    public function create()
+    {
+        return view('users.create');
+    }
+    public function store(Request $request)
+    {
+        User::create([
+            'name'=>$request->get('name'),
+            'email'=>$request->get('email'),
+            'password'=>Hash::make($request->get('password')),
+            'role_id'=>$request->get('role_id'),
+
+        ]);
+        return to_route('users');
+
+    }
+    public function destory($userid)
+    {
+        $user = User::find($userid);
+        $user->delete();
+       return to_route('users');
+
+    }
+    public function show($userid)
+    {
+        $user = User::find($userid);
+        return view('users.view', ['user' => $user]);
+
+
+    }
+    
+}
+
+*/ 
