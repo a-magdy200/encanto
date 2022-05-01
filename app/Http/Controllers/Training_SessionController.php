5702c4664a3a60bfb6e865fc43a1a2397f92e55e
ayaahassan <eng.ayaa2050@gmail.com>
2022-05-01 12:22:11 +0200

    trainingsessions views

--- app/Http/Controllers/Training_SessionController.php
+++ app/Http/Controllers/Training_SessionController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\TrainingSession;
 class Training_SessionController extends Controller
 {
-    //
+    public function index()
+    {
+        $trainingSessions = TrainingSession::all('id', 'user_id', 'package_id', 'number_of_sessions', 'price')->toArray();
+        $Headings = ['id', 'user_id', 'package_id', 'number_of_sessions', 'price'];
+        $Title = 'orders';
+        return view('trainingSessions.index')->with(['items' => $trainingSessions, 'title' => $Title, 'headings' => $Headings]);
+    }
 }
---
+++ resources/views/trainingSessions/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="text-center">
+  <a href="{{ route('trainingSession.create') }}" class="mt-4 btn btn-success">Create </a>
+</div><br>
+<x-table-component resource="users" :bannable="true" :actions="true"  title="{{$title}}" :headings="$headings" :items="$items"/>
+
+@endsection
