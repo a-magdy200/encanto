9308e501c36d0f9e50f4c15516e0b9d4b5f5660d
ayaahassan <eng.ayaa2050@gmail.com>
2022-05-01 22:53:58 +0200

    add buttons actions

--- resources/views/components/table-component.blade.php
+++ resources/views/components/table-component.blade.php
@@ -36,7 +36,7 @@
                                 @endforeach
                                 @if($actions)
                                     <td class="d-flex align-items-center">
-                                        <a href="{{$resource}}/{{$item['id']}}" class="btn btn-info"><i class="fa fa-eye"></i></a>
+                                       <a href="{{$resource}}/{{$item['id']}}" class="btn btn-info"><i class="fa fa-eye"></i></a>
                                         <a href="{{$resource}}/{{$item['id']}}/edit" class="btn btn-warning mx-2"><i class="fa fa-edit"></i></a>
                                         <a href="{{$resource}}/{{$item['id']}}/delete" class="btn btn-danger"><i class="fa fa-times"></i></a>
                                     </td>
