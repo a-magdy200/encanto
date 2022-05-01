ba9cf135410ba07b4be9eda4331b70da1b402dba
ayaahassan <eng.ayaa2050@gmail.com>
2022-05-01 22:29:43 +0200

    edit table resource

--- resources/views/trainingSessions/index.blade.php
+++ resources/views/trainingSessions/index.blade.php
@@ -4,6 +4,6 @@
 <div class="text-center">
   <a href="{{ route('trainingSessions.create') }}" class="mt-4 btn btn-success">Create </a>
 </div><br>
-<x-table-component resource="users" :bannable="true" :actions="true"  title="{{$title}}" :headings="$headings" :items="$items"/>
+<x-table-component resource="trainingSessions" :bannable="true" :actions="true"  title="{{$title}}" :headings="$headings" :items="$items"/>
 
 @endsection
