8f4bebc697fb7fd6b71001e95acf275f5dc85076
Ahmed Magdy <ahmedmagdy.200m@gmail.com>
2022-05-01 20:52:55 +0200

    Added gym, city seeders and factories

---
+++ database/factories/GymFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Gym>
+ */
+class GymFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->company(),
+            'city_id' => rand(1, 30)
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,9 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
             RoleSeeder::class,
-            AdminSeeder::class
+            AdminSeeder::class,
+            CitySeeder::class,
+            GymSeeder::class
         ]);
     }
 }
