bb0e6a1fb01b80d82a8ae3aa8898b9364496fb5b
reham15 <rehamnader0123@gmail.com>
2022-05-01 12:28:41 +0200

    add attendence

---
+++ resources/views/attendance.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+@section('content-header')
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>DataTables</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">DataTables</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+        @endsection
+        @section('content')
+            <x-table-component resource="users" :bannable="true" :actions="true" title="{{$title}}" :headings="$headings" :items="$items"/>
+@endsection
