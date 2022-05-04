5d994e2538c5f6cd96cb4fa7a41c4c234c3918ef
git-trial <rowan.rowanahmedali11@gmail.com>
2022-05-04 18:08:03 +0200

    add imagr in city manager

--- resources/views/citymanagers/show.blade.php
+++ resources/views/citymanagers/show.blade.php
@@ -24,7 +24,7 @@
                 <div class="card card-primary card-outline ">
                     <div class="card-body box-profile">
                         <div class="text-center">
-                            <img class="profile-user-img img-fluid img-circle" src="../../dist/img/user4-128x128.jpg"
+                            <img class="profile-user-img img-fluid img-circle" src="{{asset('storage/imaages' .$user->avatar)}}"
                                 alt="User profile picture">
                         </div>
 
