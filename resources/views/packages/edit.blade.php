3ed9aaffcfefd9318d1f055734a664baa4a588fe
S-Mokhtar <shaimaamukhtar177@gmail.com>
2022-05-04 17:57:14 +0200

    final edit

--- resources/views/packages/edit.blade.php
+++ resources/views/packages/edit.blade.php
@@ -7,15 +7,15 @@
     @csrf
     <div class="mb-3">
         <label for="exampleFormControlInput1" class="form-label">Package Name</label>
-        <input name="package_name" type="text" class="form-control" id="exampleFormControlInput1" placeholder="{{ $packages->package_name}}">
+        <input name="package_name" type="text" class="form-control" id="exampleFormControlInput1" value="{{ $packages->package_name}}">
     </div>
     <div class="mb-3">
         <label for="exampleFormControlTextarea1" class="form-label">Session Numbers</label>
-        <input name="number_of_sessions" class="form-control" id="exampleFormControlTextarea1" placeholder="{{ $packages->number_of_sessions}}">
+        <input name="number_of_sessions" class="form-control" id="exampleFormControlTextarea1" value="{{ $packages->number_of_sessions}}">
     </div>
     <div class="mb-3">
         <label for="exampleFormControlTextarea1" class="form-label">Price in cents</label>
-        <input name="price" type="text" class="form-control" id="exampleFormControlTextarea1" placeholder="{{ $packages->price}}">
+        <input name="price" type="text" class="form-control" id="exampleFormControlTextarea1" value="{{ $packages->price}}">
     </div>
     @method('PUT')
     <button class="btn btn-primary">Update</button>
