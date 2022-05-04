b84fb959f6aa3081323b4ee09f5a3ded89b62853
Ahmed Magdy <ahmedmagdy.200m@gmail.com>
2022-05-04 15:41:04 +0200

    Update training package relation with gym

--- app/Models/TrainingPackage.php
+++ app/Models/TrainingPackage.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class TrainingPackage extends Model
@@ -13,9 +14,14 @@ class TrainingPackage extends Model
         'package_name',
         'number_of_sessions',
         'price',
+        'gym_id'
     ];
     public function orders(): HasMany
     {
         return $this->hasMany(Order::class);
+    }
+    public function gym(): BelongsTo
+    {
+        return $this->belongsTo(Gym::class);
     }
 }
