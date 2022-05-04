9e0c047d8465ee1205f43bef2208d8128afdc1ee
ayaahassan <eng.ayaa2050@gmail.com>
2022-05-04 02:53:36 +0200

    add session validation-profile views

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -9,14 +9,14 @@
               <tr>
                  
                   <td>{{$item->id}}</td>
-                  <td>{{$item->client->user_id}}</td>
+                  <td>{{$item->client}}</td>
                   <td>{{$item->package->package_name}}</td>
                   <td>{{$item->number_of_sessions}}</td>
                   <td>{{$item->price}}</td>
                   <td class="d-flex align-items-center">
-                      <a href="trainingSessions/{{$item['id']}}" class="btn btn-info"><i class="fa fa-eye"></i></a>
-                      <a href="trainingSessions/{{$item['id']}}/edit" class="btn btn-warning mx-2"><i class="fa fa-edit"></i></a>
-                      <a href="trainingSessions/{{$item['id']}}/delete" class="btn btn-danger delete-btn" data-toggle="modal" data-target="#delete-modal"><i class="fa fa-times"></i></a>
+                      <a href="orders/{{$item['id']}}" class="btn btn-info"><i class="fa fa-eye"></i></a>
+                      <a href="orders/{{$item['id']}}/edit" class="btn btn-warning mx-2"><i class="fa fa-edit"></i></a>
+                      <a href="orders/{{$item['id']}}/delete" class="btn btn-danger delete-btn" data-toggle="modal" data-target="#delete-modal"><i class="fa fa-times"></i></a>
                   </td>
               </tr>
           @endforeach
